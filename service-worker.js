a6849f5e391254f746403a825aa1e1334514fa89
Max Marquis <maxim.marquis@gmail.com>
2021-09-17 21:27:22 -0400

    service-worker

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@ const CACHE_MYSHOP = 'static-cache-v1.1';
 //Add list of files to cache here.
 
 const FILES_TO_CACHE = [
-    './index.html','./compte.html','./inscription.html','./panier.html','./produit.html','./img/13408200.jpg','./img/15337847.jpg','./img/15337850.jpg','./img/15387989.jpg','./img/15427808.jpg','./img/15442045.jpg','./img/15442045.jpg','./img/di-20210910-offer-cameras-top-deals-m.jpg','./img/ht-20210910-offer-tv-topdeal-m.jpg','./img/pa-20210910-offer-td-pa-on-sale-m.jpg','./img/recu.png','./img/souris.png','./img/pwa/souris.png','./img/pwa/maskable_icon.png','./img/pwa/unnamed.png'
+    
 ];
 
 self.addEventListener('install', (evt) => {
